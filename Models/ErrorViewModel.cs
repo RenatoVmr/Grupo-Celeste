b2892de2cd294f22f9433a1ac3c1053cac5f8a74
RenatoVmr <Renato_vargas2@usmp.pe>
2025-08-22 20:50:36 -0500

    Proyecto base Grupo-Celeste

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Grupo_Celeste.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
